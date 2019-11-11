380644b27306dacee3673b2e6c2b5b80d49930fe
Иван Тимашков <timscriptov@mail.ru>
2019-11-11 12:57:11 +0300

    Upload files

---
+++ lib-android-compiler/src/main/java/com/duy/android/compiler/project/IProjectManager.java
@@ -0,0 +1,10 @@
+package com.duy.android.compiler.project;
+
+import android.support.annotation.NonNull;
+
+import java.io.File;
+
+public interface IProjectManager<T extends JavaProject> {
+    @NonNull
+    T loadProject(File rootDir, boolean tryToImport) throws Exception;
+}
